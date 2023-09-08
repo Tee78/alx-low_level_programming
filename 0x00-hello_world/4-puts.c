f25732eb897180fd5c16c58e7c68f2ec958089c0
Tee78 <ajokeadeoluwa971@gmail.com>
2023-09-08 03:24:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,23 @@
 #!/bin/bash
 #include <stdio.h>
+
+
 /**
+
  * main - this is the main put function
+
  * Return: 0
+
  */
+
 int main(void)
+
 {
+
         puts("\"Programming is like building a multilingual puzzle");
+
                 return (0);
+
 }
 
 
