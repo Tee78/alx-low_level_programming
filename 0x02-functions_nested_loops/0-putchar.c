f4ed1d2340c8261fc6f493a3b04f8721bae940e8
Tee78 <ajokeadeoluwa971@gmail.com>
2023-09-12 09:32:38 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("_putchar\n");  // Print "_putchar" and then go to the next line
+    return 0;  // Indicate the program finished without any issues
+}
+
