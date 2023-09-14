36ab93e8b0045c8441ca7185a7bbf78b5d27130d
Tee78 <ajokeadeoluwa971@gmail.com>
2023-09-13 21:48:46 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,43 +1,90 @@
 #include "main.h"
 
-#include <stdio.h>
-
 /**
 
- * print_to_98 - prints all natural numbers from n to 98
+ * print_times_table - prints the n times table, starting with 0
 
- *
-
- * @n: starting point
-
- *
-
- * Return: returns nothing
-
- *
+ * @n: number of the times table
 
  */
 
-void print_to_98(int n)
+void print_times_table(int n)
 
 {
 
-        if (n <= 98)
+        int i, j, k;
+
+
+        if (n >= 0 && n <= 15)
 
         {
 
-                for (n = n; n <= 97; n++)
+                for (i = 0; i <= n; i++)
 
-                        printf("%d, ", n);
+                {
+
+                        for (j = 0; j <= n; j++)
+
+                        {
+
+                                k = j * i;
+
+                                if (j == 0)
+
+                                {
+
+                                        _putchar(k + '0');
+
+                                } else if (k < 10 && j != 0)
+
+                                {
+
+                                        _putchar(',');
+
+                                        _putchar(' ');
+
+                                        _putchar(' ');
+
+                                        _putchar(' ');
+
+                                        _putchar(k + '0');
+
+                                } else if (k >= 10 && k < 100)
+
+                                {
+
+                                        _putchar(',');
+
+                                        _putchar(' ');
+
+                                        _putchar(' ');
+
+                                        _putchar((k / 10) + '0');
+
+                                        _putchar((k % 10) + '0');
+
+                                } else if (k >= 100)
+
+                                {
+
+                                        _putchar(',');
+
+                                        _putchar(' ');
+
+                                        _putchar((k / 100) + '0');
+
+                                        _putchar(((k / 10) % 10) + '0');
+
+                                        _putchar((k % 10) + '0');
+
+                                }
+
+                        }
+
+                        _putchar('\n');
+
+                }
 
         }
-
-        else
-
-                for (n = n; n > 98; n--)
-
-                        printf("%d, ", n);
-
-        printf("98\n");
 
 }
