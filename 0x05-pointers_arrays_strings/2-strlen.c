9825c106ef172f4d876d2fd3c2826f0cc0a383aa
Tee78 <ajokeadeoluwa971@gmail.com>
2023-09-19 03:47:26 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,10 +8,11 @@
 int _strlen(char *s)
 {
 	int p = 0;
-	/incremeant up to when the last character is NULL,\0/
-	while (*(s + p) != 0)
+
+	while (*s != '\0')
 	{
 		p++;
+		s++;
 	}
 	return (p);
 }
