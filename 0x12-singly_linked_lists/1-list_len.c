af7021566d7d0ad577c77c70e5ca3c3dbdec6e83
Tee78 <ajokeadeoluwa971@gmail.com>
2023-10-22 04:34:49 -0700

    linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,18 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - returns the elements number in a linked list
- * @h: pointer to the list_t list node
- *
- * Return: number of elements in h
+ * list_len - list length
+ * @h: pointer to the node.that is first
+ * Return: nodes number in the list
  */
 size_t list_len(const list_t *h)
 {
-size_t n = 0;
+	int i;
 
-while (h)
-{
-n++;
-h = h->next;
-}
-return (n);
+	if (h == NULL)
+	return (0);
+
+	for (i = 1; h->next != NULL; i++)
+		h = h->next;
+	return (i);
 }
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+int len(const char *str);
+/**
+ * add_node - describes add a node to the start of list
+ * @head: points to the list head
+ * @str: string to be inserted at the first place
+ * Return: pointer to the first node (newly assigned)
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+	{
+		new_node->next = *head;
+		new_node->str = strdup(str);
+		new_node->len = len(str);
+		*head = new_node;
+	}
+	return new_node;
+}
+/**
+ * len - describes the length of str (string)
+ * @str: describes the string to length
+ * Return: string length
+ */
+int len(const char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return (0);
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
