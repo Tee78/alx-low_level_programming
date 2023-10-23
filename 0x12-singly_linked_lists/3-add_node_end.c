8ca3dcd2fbf2d43d9d8d7033db043e9f7d7ddeb5
Tee78 <ajokeadeoluwa971@gmail.com>
2023-10-23 00:35:41 -0700

    singly linked list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,69 @@
+#include "lists.h"
+int len(const char *str);
+list_t *create_node(const char *str);
+/**
+ * add_node_end - it adds node to the list end
+ * @head: points to the first node
+ * @str: the new node is added at the end
+ * Return: pointer to the head
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *tmp;
+	list_t *new_node;
+
+	tmp = *head;
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+		new_node = create_node(str);
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+
+	{
+		*head = new_node;
+		return (*head);
+	}
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new_node;
+	return (*head);
+}
+/**
+ * create node: it declares or create node
+ * @str: string inputed in the new node
+ * Return: the pointer to the malloced memory
+ */
+list_t *create_node(const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = len(str);
+	new_node->next = NULL;
+	return (new_node);
+}
+/**
+ * len - string length
+ * @str: string length must be found
+ * Return: length amount
+ */
+int len(const char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return (0);
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
